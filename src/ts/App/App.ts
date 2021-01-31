10dcb6e5745004548ac4f99dab1e672c4cc10c3c
kacus <54528490+kacus@users.noreply.github.com>
2021-01-31 18:41:54 +0100

    fix in use of pocess.env

--- src/ts/App/App.ts
+++ src/ts/App/App.ts
@@ -1,9 +1,9 @@
 import BoardController from "./Controllers/boardController";
 
 export default function App(): void {
-    if (+process.env.DEBUG) console.log('Start Chess App...');
+    if (!!process.env.DEBUG) console.log('Start Chess App...');
     const root = document.getElementById('root');
 
-    const controller = new BoardController(root);
+    const controller = new BoardController(root!);
     controller.setBoard();
 }
