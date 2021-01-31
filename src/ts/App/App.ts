7cd3d7537de983c035cac1fa4dd4fb7f7041ff2b
kacus <54528490+kacus@users.noreply.github.com>
2021-01-31 17:36:45 +0100

    Use .env instead of variable

--- src/ts/App/App.ts
+++ src/ts/App/App.ts
@@ -1,8 +1,7 @@
 import BoardController from "./Controllers/boardController";
 
-
-export default function App(debug: boolean = false): void {
-    if (debug) console.log('Start Chess App...');
+export default function App(): void {
+    if (+process.env.DEBUG) console.log('Start Chess App...');
     const root = document.getElementById('root');
 
     const controller = new BoardController(root);
