e1d7d89c86d6013a9904f58ff784ebce5deea565
kacus <54528490+kacus@users.noreply.github.com>
2021-02-01 18:28:27 +0100

    Solve using process.env.variables

--- src/ts/App/App.ts
+++ src/ts/App/App.ts
@@ -1,7 +1,7 @@
 import BoardController from "./Controllers/boardController";
 
 export default function App(): void {
-    if (!!process.env.DEBUG) console.log('Start Chess App...');
+    if (+process.env.DEBUG!) console.log('Start Chess App...');
     const root = document.getElementById('root')!;
 
     const controller = new BoardController(root);
