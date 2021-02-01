050d1e152c92197111ec2f37e3a834b0954aade1
kacus <54528490+kacus@users.noreply.github.com>
2021-02-01 18:26:36 +0100

    Move exclamation mark to correct place

--- src/ts/App/App.ts
+++ src/ts/App/App.ts
@@ -2,8 +2,8 @@ import BoardController from "./Controllers/boardController";
 
 export default function App(): void {
     if (!!process.env.DEBUG) console.log('Start Chess App...');
-    const root = document.getElementById('root');
+    const root = document.getElementById('root')!;
 
-    const controller = new BoardController(root!);
+    const controller = new BoardController(root);
     controller.setBoard();
 }
