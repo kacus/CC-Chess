43762ee4b984263a20f5d09d398ff4a4cd0a6621
pkol92 <paulina_kolasa@wp.pl>
2021-02-02 12:54:24 +0100

    add menu view

--- src/ts/App/Views/menu.ts
+++ src/ts/App/Views/menu.ts
@@ -11,52 +11,76 @@ export default class MenuView implements IMenu {
             element.classList.add(className);
         }
         return element;
-    }
+    };
 
-    // appendEl(el: HTMLElement, child1: HTMLElement, child2?: HTMLElement, child3?: HTMLElement, child4?: HTMLElement) {
-    //     el.appendChild(child1);
-    //     el.appendChild(child2);
-    //     el.appendChild(child3);
-    //     el.appendChild(child4);
-    // }
-
-
-    display(){
+    createMenuWraper(element: HTMLElement){
+        const menu_wraper = this.createElement('div', 'menu_wraper');
+        this.createMenuBackground(menu_wraper);
+        this.createBtnBox(menu_wraper);
+        element.appendChild(menu_wraper);
+    };
+       
+    createMenuBackground(element: HTMLElement){
         const menu_background = this.createElement('div', 'menu_background');
-
         const setting_players = this.createElement('div', 'setting_players');
-        setting_players.innerText = 'USTAWIENIA GRACZY';
+        const setting_players_text = this.createElement('div', 'setting_players_text');
+        setting_players_text.innerText = 'USTAWIENIA GRACZY';
+        setting_players.appendChild(setting_players_text);
+        menu_background.appendChild(setting_players);
 
+        this.createPlayersBox(menu_background);
+        this.createRadio(menu_background);
+        this.createSettingBox(menu_background);
+        element.appendChild(menu_background);
+    };
+
+    createBtnBox(element: HTMLElement){
+        const btn_box = this.createElement('div', 'btn_box');
+        const btn_start = this.createElement('button', 'btn_start');
+        btn_start.innerText = "ROZPOCZNIJ GRĘ";
+        const btn_load = this.createElement('button', 'btn_load');
+        btn_load.innerText = "WCZYTAJ GRĘ";
+        btn_box.append(btn_start, btn_load);
+        element.appendChild(btn_box);
+    };
+
+    createPlayersBox(element: HTMLElement){
         const players_box = this.createElement('div', 'players_box');
-
-        const players_name = this.createElement('div', 'players_name');
-        players_name.innerText = "IMIONA GRACZY";
+        const players_box_text = this.createElement('div', 'players_box_text');
+        players_box_text.innerText = "IMIONA GRACZY";
+        const players = this.createElement('div', 'players');
 
         const first_player = this.createElement('div', 'one_player');
-        first_player.innerText = 'Białe | Nazwa gracza'
+        const white = this.createElement('div', 'white_player');
+        white.innerText = 'Białe | Nazwa gracza'
         const insert_name_first = this.createElement('input', 'insert_name');
         insert_name_first.setAttribute('type', 'text');
         insert_name_first.setAttribute('maxlength', '20');
         insert_name_first.setAttribute('pattern', '[a-zA-Z0-9]{3,20}');
-        first_player.appendChild(insert_name_first);
+        first_player.append(white, insert_name_first);
 
         const second_player = this.createElement('div', 'one_player');
-        second_player.innerText = 'Czarne | Nazwa gracza'
+        const black = this.createElement('div', 'black_player');
+        black.innerText = 'Czarne | Nazwa gracza'
         const insert_name_second = this.createElement('input', 'insert_name');
         insert_name_second.setAttribute('type', 'text');
         insert_name_second.setAttribute('maxlength', '20');
         insert_name_second.setAttribute('pattern', '[a-zA-Z0-9]{3,20}');
-        second_player.appendChild(insert_name_second);
+        second_player.append(black, insert_name_second);
 
-        players_box.appendChild(players_name);
-        players_box.appendChild(first_player);
-        players_box.appendChild(second_player);
+        players.append(first_player, second_player);
+        players_box.append(players_box_text, players);
+        element.appendChild(players_box);
+    };
 
+    createRadio(element: HTMLElement) {
         const form_radio = this.createElement('form', 'form_radio');
+
         const radio_color = this.createElement('input');
         radio_color.setAttribute('type','radio');
         radio_color.setAttribute('id','random color');
         radio_color.setAttribute('value','random color');
+
         const label_color = this.createElement('label');
         label_color.setAttribute('for','random color');
         label_color.innerText = 'Losowy kolor';
@@ -65,19 +89,26 @@ export default class MenuView implements IMenu {
         radio_move.setAttribute('type','radio');
         radio_move.setAttribute('id','possible move');
         radio_color.setAttribute('value','possible move');
+
         const label_move = this.createElement('label');
         label_move.setAttribute('for','possible move');
         label_move.innerText = 'Pokazuj możliwe posunięcia';
 
-        form_radio.appendChild(radio_color);
-        form_radio.appendChild(label_color);
-        form_radio.appendChild(radio_move);
-        form_radio.appendChild(label_move);
-        // this.appendEl(form_radio, radio_color, label_color, radio_move, label_move);
+        form_radio.append(radio_color, label_color, radio_move, label_move);
+        element.appendChild(form_radio);
+    };
 
+    createSettingBox(element: HTMLElement){
         const setting_box = this.createElement('div', 'setting_box');
+        this.createSettingGame(setting_box);
+        this.createTimeGame(setting_box);
+        element.appendChild(setting_box);
+    };
+
+    createSettingGame(element: HTMLElement){
         const setting_game = this.createElement('div', 'setting_game');
-        setting_game.innerText ='USTAWIENIA GRY';
+        const setting_text = this.createElement('div', 'setting_text');
+        setting_text.innerText ='USTAWIENIA GRY';
 
         const form_action = this.createElement('form', 'form_action');
         form_action.innerText = "WARIANT GRY";
@@ -91,74 +122,62 @@ export default class MenuView implements IMenu {
         option_second.setAttribute('value','amateur');
         option_second.innerText ='Amatorski';
 
-        select.appendChild(option_first);
-        select.appendChild(option_second);
-        // this.appendEl(select, option_first, option_second)
+        select.append(option_first, option_second);
         form_action.appendChild(select);
+        setting_game.append(setting_text, form_action);
+        element.appendChild(setting_game);
+    };
 
+    createTimeGame(element: HTMLElement){
         const time_game = this.createElement('div', 'tame_game');
         time_game.innerText = 'CZAS GRY'
+
         const span_tg = this.createElement('span');
         span_tg.innerText = 'W MINUTACH';
+
         time_game.appendChild(span_tg);
+
         const slider_box = this.createElement('div', 'slider_box');
         const slider = this.createElement('input', 'slider_range');
         slider.setAttribute('type', 'range');
+        slider.setAttribute('name', 'range_time');
         slider.setAttribute('min', '2');
         slider.setAttribute('max', '15');
-        slider.setAttribute('step', '0.1');
+        slider.setAttribute('step', '1');
         slider.setAttribute('value', '5');
         slider.setAttribute('id', 'range');
 
         const slider_time = this.createElement('output');
         slider_time.setAttribute('id', 'range_display');
-        slider.appendChild(slider_time);
-        slider_box.appendChild(slider);
+        slider_time.setAttribute('name', 'range_display');
+        slider_time.setAttribute('for', 'range_time');
+        slider_time.textContent = `Wybrany czas gry to: ${slider.getAttribute('value')}min`
 
-        // const x = document.querySelector("input");
-        // const y = document.getElementById("range_display");
-        // y.innerHTML = x.value;
+        slider_box.append(slider, slider_time);
 
-        function addTime() {
-            const x = document.querySelector('input').value;
-            slider_time.innerText = `Wybrany czas gry to: ${x}min`;
-        };
+        element.append(time_game, slider_box);
+    };
 
-
+    createAddEventListner() {
         const t = document.getElementById('range');
         t.addEventListener("input", () => {
-            addTime();
+            this.addTime();
         });
-       
-     
-        setting_box.appendChild(setting_game);
-        setting_box.appendChild(form_action);
-        setting_box.appendChild(time_game);
-        setting_box.appendChild(slider_box);
-        // this.appendEl(setting_box, setting_game, form_action, time_game, slider_box);
-
-        const btn_box = this.createElement('div', 'btn_box');
-        const btn_start = this.createElement('button', 'btn_start');
-        btn_start.innerText = "ROZPOCZNIJ GRĘ";
-        const btn_load = this.createElement('button', 'btn_load');
-        btn_load.innerText = "WCZYTAJ GRĘ";
-        btn_box.appendChild(btn_start);
-        btn_box.appendChild(btn_load);
-        // this.appendEl(btn_box, btn_start, btn_load);
-
-        menu_background.appendChild(setting_players);
-        menu_background.appendChild(players_box);
-        menu_background.appendChild(form_radio);
-        menu_background.appendChild(setting_box);
-        // this.appendEl(menu_background, setting_players, players_box, form_radio, setting_box)
-
-        const menu_wraper = this.createElement('div', 'menu_wraper');
-
-        menu_wraper.appendChild(menu_background);
-        menu_wraper.appendChild(btn_box);
-        // this.appendEl(menu_wraper, menu_background, btn_box);
-
-        this.parent.appendChild(menu_wraper);
+        t.addEventListener("change", () => {
+            this.addTime()
+        });
     }
 
-}
+    addTime() {
+        const x = document.querySelector('.slider_box'); 
+        const z =x.querySelector('input').value  
+        const y = x.querySelector('output');    
+        y.innerText = `Wybrany czas gry to: ${z}min`;
+    };
+
+    display(){
+        this.createMenuWraper(this.parent);
+       
+        this.createAddEventListner();    
+    };
+};
