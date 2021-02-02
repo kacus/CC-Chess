be3a2a4453e9adbf5923684ec9b2444fd5bef4eb
pkol92 <paulina_kolasa@wp.pl>
2021-02-02 12:55:40 +0100

    add menu interface

--- src/ts/App/Views/menuInterface.ts
+++ src/ts/App/Views/menuInterface.ts
@@ -1,5 +1,15 @@
 export default interface IMenu {
     parent: HTMLElement;
     createElement(tag: string, className?: string);
+    createMenuWraper(element: HTMLElement);
+    createMenuBackground(element: HTMLElement);
+    createBtnBox(element: HTMLElement);
+    createPlayersBox(element: HTMLElement);
+    createRadio(element: HTMLElement);
+    createSettingBox(element: HTMLElement);
+    createSettingGame(element: HTMLElement)
+    createTimeGame(element: HTMLElement);
+    createAddEventListner();
+    addTime();
     display();
-}
+};
