c54ea413af8da5a036fb0c8298b664e7fd7ab5ed
pkol92 <paulina_kolasa@wp.pl>
2021-02-03 13:29:19 +0100

    remove methods

--- src/ts/App/Views/menuInterface.ts
+++ src/ts/App/Views/menuInterface.ts
@@ -1,15 +1,5 @@
 export default interface IMenu {
     parent: HTMLElement;
     createElement(tag: string, className?: string);
-    createMenuWraper(element: HTMLElement);
-    createMenuBackground(element: HTMLElement);
-    createBtnBox(element: HTMLElement);
-    createSettingPlayers(element: HTMLElement);
-    createRadio(element: HTMLElement);
-    createSettingBox(element: HTMLElement);
-    createSettingGame(element: HTMLElement)
-    createTimeGame(element: HTMLElement);
-    createAddEventListner();
-    addTime();
     display();
 };
