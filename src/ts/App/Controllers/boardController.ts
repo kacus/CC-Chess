3c99837d3eebd517e26b57170a95fd7c6fe948e6
kacus <54528490+kacus@users.noreply.github.com>
2021-02-05 13:07:01 +0100

    Add comments for better understanding code

--- src/ts/App/Controllers/boardController.ts
+++ src/ts/App/Controllers/boardController.ts
@@ -25,6 +25,7 @@ export default class BoardController {
         this.moveSaver = new MoveSaver();
     }
 
+    //Starts new game
     public newGame(time: number): void {
         //reset constroller
         this.moveFor = EColor.White;
@@ -48,27 +49,34 @@ export default class BoardController {
         if (+process.env.DEBUG!) console.log(`NEW GAME STARTS`);
     }
 
-    //Moves and attacks functions
+    //  Moves and attacks functions
+
+    //check if given position is on given list
     private isFieldOnList(pos: TField, list: TField[]): boolean {
         return !(list.every(elem => elem[0] !== pos[0] || elem[1] !== pos[1]));
     }
 
+    //select Figure on given position
     private selectNewFigure(pos: TField): void {
         this.view.resetStyles();
 
+        //checks moves avaible for figure on given position
         this.selectedField = pos;
         this.movesForSelected = this.board.possibleMovesFor(this.selectedField);
         this.attacksForSelected = this.board.possibleAttacksFor(this.selectedField);
 
+        //mark figure on given position as selected
         this.view.setAsSelected(pos);
         const figure = this.board.get(pos)!;
 
+        //check for castling
         if (figure.name === EFigureType.King) {
             const row = this.moveFor === EColor.White ? 1 : 8;
             if (this.canMakeCasting(this.moveFor, true)) this.movesForSelected.push([7, row]);
             if (this.canMakeCasting(this.moveFor, false)) this.movesForSelected.push([2, row]);
         }
 
+        //display on board positions avaible to move and attack
         this.movesForSelected.forEach(field => {
             this.view.setAsPossibleToMove(field);
         });
@@ -77,6 +85,7 @@ export default class BoardController {
         });
     }
 
+    //cancel selection
     private resetSelectedFigure() {
         this.view.resetStyles();
         this.selectedField = null;
@@ -84,88 +93,113 @@ export default class BoardController {
         this.attacksForSelected = [];
     }
 
+    //Move given figure from start position to end position
     private makeMove(start: TField, end: TField, figure: IFigure): void {
+        //Save move
         const savedMove = new SaveOfMove(figure.color, figure, start, end);
         this.moveSaver.addMove(savedMove);
 
+        //move
         this.view.move(start, end, figure);
         this.board.move(start, end);
         this.changeTurn();
 
+        //print move
         console.log(savedMove.printMove());
     }
 
+    //Attack given figure on end position by given figure on start position
     private makeAttack(start: TField, end: TField, figure: IFigure): void {
+        //get enemy figure
         const enemyFigure = this.board.get(end)!;
+
+        //save attack
         const savedAttack = new SaveOfMove(figure.color, figure, start, end, enemyFigure);
         this.moveSaver.addMove(savedAttack);
 
+        //attack
         this.view.move(start, end, figure);
         this.board.move(start, end);
         this.changeTurn();
 
+        //print attack
         console.log(savedAttack.printMove());
     }
 
+    //Handler for clicking on field
     private clickOnField = (pos: TField): void => {
-        //We have selected figure already
         if (this.selectedField) {
+            //We have selected figure already
             this.figureAlreadySelected(pos, this.selectedField);
         } else {
+            //We didn't select figure yet
             this.figureNotSelected(pos);
         }
     }
 
+    //Help function for clickOnField()
     private figureNotSelected(pos: TField): void {
-        //We didn't select figure yet
+        //get clicked position
         const figure = this.board.get(pos);
 
-        //We clicked on our figure
+        //if we clicked on figure select this field
         if (figure && figure.color === this.moveFor) {
             this.selectNewFigure(pos);
         }
+        //otherwise do nothing
     }
 
-    private figureAlreadySelected(pos: TField, selected: TField): void {
-        const clickedFigure = this.board.get(pos);
+    //Help function for clickOnField()
+    private figureAlreadySelected(clickedPos: TField, selectedPos: TField): void {
+        //get clicked field
+        const clickedFigure = this.board.get(clickedPos);
 
-        //we clicked another figure and have selected one
-        const figure = this.board.get(selected);
+        //get already clicked figure
+        const figure = this.board.get(selectedPos);
+
+        //check if we clicked figure
         if (clickedFigure && figure) {
-            //it is our figure
+            //check if it is our figure
             if (clickedFigure.color === figure.color) {
-                this.selectNewFigure(pos);
+                //It is our figure so select new one
+                this.selectNewFigure(clickedPos);
                 return;
             } else {
                 //it is enemy figure
-                //we can attack this figure
-                if (this.isFieldOnList(pos, this.attacksForSelected)) {
-                    this.makeAttack(selected, pos, figure);
+                //check if this figure is in renge already selected figure
+                if (this.isFieldOnList(clickedPos, this.attacksForSelected)) {
+                    //It is so we can perform attack
+                    this.makeAttack(selectedPos, clickedPos, figure);
                 }
             }
-        } else if (figure && this.isFieldOnList(pos, this.movesForSelected)) {
+        //check if we clicked on field we can move
+        } else if (figure && this.isFieldOnList(clickedPos, this.movesForSelected)) {
             //we clicked empty field
             //we can move on this field
+
+            //check for king
             if (figure.name === EFigureType.King && !figure.isMoved) {
+                //check if it is field for castling
                 const row = this.moveFor === EColor.White ? 1 : 8;
-                if (pos[0] === 7 && pos[1] === row) {
+                if (clickedPos[0] === 7 && clickedPos[1] === row) {
                     //king side castling
                     this.makeCastling(this.moveFor, true);
-                } else if (pos[0] === 2 && pos[1] === row) {
+                } else if (clickedPos[0] === 2 && clickedPos[1] === row) {
                     //queen side castling
                     this.makeCastling(this.moveFor, false);
                 } else {
                     //normal move
-                    this.makeMove(selected, pos, figure);
+                    this.makeMove(selectedPos, clickedPos, figure);
                 }
             } else {
                 //normal move
-                this.makeMove(selected, pos, figure);
+                this.makeMove(selectedPos, clickedPos, figure);
             }
         }
         this.resetSelectedFigure();
     }
 
+    //Make castling, should be used only if canMakeCastling gives true for same parameters
     private makeCastling(color: EColor, kingSide: boolean) {
         //save castling
         const savedMove = new SaveOfCastling(color, kingSide);
@@ -192,22 +226,25 @@ export default class BoardController {
         this.board.move(initRookPos, newRookPos);
         rook.move();
 
-
+        //change turn for enemy
         this.changeTurn();
     }
 
+    //Check if castling can be performed
     private canMakeCasting(color: EColor, kingSide: boolean): boolean {
+        //Select row for king
         const row = color === EColor.White ? 1 : 8;
+        //select king figure
         const king = this.board.get([5, row]);
         if (king && !king.isMoved) {
             if (kingSide) {
-                //king side castling
+                //check king side castling
                 if (this.board.get([6, row])) return false;
                 if (this.board.get([7, row])) return false;
                 const rook = this.board.get([8, row]);
                 if (rook && !rook.isMoved) return true;
             } else {
-                //queen side castling
+                //check queen side castling
                 if (this.board.get([4, row])) return false;
                 if (this.board.get([3, row])) return false;
                 if (this.board.get([2, row])) return false;
@@ -215,23 +252,27 @@ export default class BoardController {
                 if (rook && !rook.isMoved) return true;
             }
         }
-
         return false;
     }
 
-    //Game functions
+    //  Game functions
+
+    //display board for given model
     private setUpBoard(board: BoardModel): void {
         this.view.setUpBoard(board);
     }
 
+    // set up new timer
     private setUpTimer() {
         this.timer = setInterval(this.updateTime, 1000);
     }
 
+    // reset timer
     private stopTimer() {
         clearInterval(this.timer);
     }
 
+    //handler for timer
     private updateTime = (): void => {
         if (this.moveFor === EColor.White) {
             this.timeLeftForWhite -= 1;
@@ -250,10 +291,12 @@ export default class BoardController {
         }
     };
 
+    //change turn for enemy player
     private changeTurn() {
         this.moveFor = this.moveFor === EColor.White ? EColor.Black : EColor.White;
     }
 
+    //handler for 'undo' last move
     public undoMove = () => {
         if(this.moveSaver.canUndoMove()){
             this.moveSaver.revertLastMove(this.board, this.view);
@@ -262,6 +305,7 @@ export default class BoardController {
         }
     }
 
+    //game over handler
     private gameOver(winer: EColor) {
         this.stopTimer();
         if (+process.env.DEBUG!) console.log(`WINER! ${winer}`);
