c074243d62b9f8cfad396a1f2fcdbf30e1360b05
cwaniakobezczel <70508434+cwaniakobezczel@users.noreply.github.com>
2021-02-12 23:18:10 +0100

    Fixing errors and making code easier to read

--- src/ts/App/Views/tabsView.ts
+++ src/ts/App/Views/tabsView.ts
@@ -2,15 +2,20 @@ import MenuView from "./menu";
 import MovesList from "./movesList";
 
 export default class TabsView {
-  createElement(tag: string, className?: string) {
+  public init(parent: HTMLElement) {
+    const tabContainer = this.createElement("div", "tab__container");
+    parent.appendChild(tabContainer);
+    this.createNavigation(tabContainer);
+    this.createTabs(tabContainer);
+  }
+  private createElement(tag: string, className?: string) {
     const element = document.createElement(tag);
     if (className) {
       element.classList.add(className);
     }
     return element;
   }
-  public init(parent: HTMLElement) {
-    const tabContainer = this.createElement("div", "tab__container");
+  private createNavigation(tabContainer: HTMLElement) {
     const ul = this.createElement("ul", "nav");
     ul.id = "nav__tab";
     ul.addEventListener("click", this.onTabClick, false);
@@ -26,7 +31,36 @@ export default class TabsView {
     const a3 = this.createElement("a");
     a3.setAttribute("href", "#game");
     a3.innerText = "Game";
+    this.addIcons(li1, li2, li3);
 
+    tabContainer.appendChild(ul);
+    ul.appendChild(li1);
+    ul.appendChild(li2);
+    ul.appendChild(li3);
+    li1.appendChild(a1);
+    li2.appendChild(a2);
+    li3.appendChild(a3);
+  }
+
+  private createTabs(tabContainer: HTMLElement) {
+    const tabContent = this.createElement("div", "tab__content");
+    const tabPane1 = this.createElement("div", "tab__pane");
+    tabPane1.id = "moves";
+    const tabPane2 = this.createElement("div", "tab__pane");
+    tabPane2.id = "settings";
+    const tabPane3 = this.createElement("div", "tab__pane");
+    tabPane3.classList.add("active");
+    tabPane3.id = "game";
+    tabContent.appendChild(tabPane1);
+    tabContent.appendChild(tabPane2);
+    tabContent.appendChild(tabPane3);
+    tabContainer.appendChild(tabContent);
+
+    const menu = new MenuView(tabPane3);
+    menu.display();
+  }
+
+  private addIcons(li1: HTMLElement, li2: HTMLElement, li3: HTMLElement) {
     const movesIcon = this.createElement("img");
     movesIcon.setAttribute("src", `./static/assets/icons/moves_icon.svg`);
     li1.appendChild(movesIcon);
@@ -36,43 +70,15 @@ export default class TabsView {
     const gameIcon = this.createElement("img");
     gameIcon.setAttribute("src", `./static/assets/icons/game_icon.svg`);
     li3.appendChild(gameIcon);
-
-    const tabContent = this.createElement("div", "tab__content");
-    const tabPane1 = this.createElement("div", "tab__pane");
-    tabPane1.id = "moves";
-
-    const tabPane2 = this.createElement("div", "tab__pane");
-    tabPane2.id = "settings";
-    const tabPane3 = this.createElement("div", "tab__pane");
-    tabPane3.classList.add("active");
-    tabPane3.id = "game";
-
-    parent.appendChild(tabContainer);
-    tabContainer.appendChild(ul);
-    tabContainer.appendChild(tabContent);
-    ul.appendChild(li1);
-    ul.appendChild(li2);
-    ul.appendChild(li3);
-    li1.appendChild(a1);
-    li2.appendChild(a2);
-    li3.appendChild(a3);
-    tabContent.appendChild(tabPane1);
-    tabContent.appendChild(tabPane2);
-    tabContent.appendChild(tabPane3);
-
-    const menu = new MenuView(tabPane3);
-    menu.display();
-
-    // new MenuView(tabPane3);
   }
 
   public onTabClick(event: Event) {
-    let activeTabs = document.querySelectorAll(".active");
+    const aTag = <HTMLAnchorElement>event.target;
+    const activeTabs = document.querySelectorAll(".active");
     activeTabs.forEach(function (tab) {
       tab.className = tab.className.replace("active", "");
     });
-    event.target.parentElement.className += " active";
-    document.getElementById(event.target.href.split("#")[1])!.className +=
-      " active";
+    aTag.parentElement!.className += " active";
+    document.getElementById(aTag.href.split("#")[1])!.className += " active";
   }
 }
