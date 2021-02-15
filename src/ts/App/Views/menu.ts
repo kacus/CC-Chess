1e0c5eda604b60f9f527caf738fdeda94a14deeb
pkol92 <paulina_kolasa@wp.pl>
2021-02-15 22:03:45 +0100

    change pl to eng

--- src/ts/App/Views/menu.ts
+++ src/ts/App/Views/menu.ts
@@ -31,7 +31,7 @@ export default class MenuView implements IMenu {
         const btn_box = this.createElement('div', 'menu__button');
         const btn_start = this.createElement('button', 'menu__button--start');
         btn_start.setAttribute('id', "menu__button--start");
-        btn_start.innerText = "ROZPOCZNIJ GRĘ";
+        btn_start.innerText = "START THE GAME";
         btn_box.append(btn_start);
         element.appendChild(btn_box);
     };
@@ -39,17 +39,17 @@ export default class MenuView implements IMenu {
     private createSettingPlayers(element: HTMLElement){
         const setting_players = this.createElement('div', 'setting__players');
         const setting_players_text = this.createElement('div', 'setting__players--text');
-        setting_players_text.innerText = 'USTAWIENIA GRACZY';
+        setting_players_text.innerText = 'PLAYERS SETTING';
         setting_players.appendChild(setting_players_text);
         
         const players_box = this.createElement('div', 'setting__players--box');
         const players_box_text = this.createElement('div', 'setting__players--text');
-        players_box_text.innerText = "IMIONA GRACZY";
+        players_box_text.innerText = "PLAYERS NAMES";
         const players = this.createElement('div', 'setting__players--players');
 
         const first_player = this.createElement('div', 'players--one_player');
         const white = this.createElement('div', 'white_player');
-        white.innerText = 'Białe | Nazwa gracza'
+        white.innerText = "Black | Player's name"
         const insert_name_first = this.createElement('input', 'insert__name');
         insert_name_first.setAttribute('type', 'text');
         insert_name_first.setAttribute('maxlength', '20');
@@ -58,7 +58,7 @@ export default class MenuView implements IMenu {
 
         const second_player = this.createElement('div', 'players--one_player');
         const black = this.createElement('div', 'black_player');
-        black.innerText = 'Czarne | Nazwa gracza'
+        black.innerText = "White | Player's name"
         const insert_name_second = this.createElement('input', 'insert__name');
         insert_name_second.setAttribute('type', 'text');
         insert_name_second.setAttribute('maxlength', '20');
@@ -83,7 +83,7 @@ export default class MenuView implements IMenu {
 
         const label_color = this.createElement('label', 'checkbox');
         label_color.setAttribute('for','random color');
-        label_color.innerText = 'Losowy kolor';
+        label_color.innerText = 'Random color';
 
         const checkbox_move = this.createElement('input');
         checkbox_move.setAttribute('type','checkbox');
@@ -93,7 +93,7 @@ export default class MenuView implements IMenu {
 
         const label_move = this.createElement('label', 'checkbox');
         label_move.setAttribute('for','possible move');
-        label_move.innerText = 'Pokazuj możliwe posunięcia';
+        label_move.innerText = 'Show possible moves';
 
         label_color.append(checkbox_color, span_color);
         label_move.append(checkbox_move, span_move);
@@ -111,19 +111,19 @@ export default class MenuView implements IMenu {
     private createSettingGame(element: HTMLElement){
         const setting_game = this.createElement('div', 'setting--game');
         const setting_text = this.createElement('div', 'setting__game--text');
-        setting_text.innerText ='USTAWIENIA GRY';
+        setting_text.innerText ='GAME SETTINGS';
 
         const form_action = this.createElement('form', 'setting__box--form__action');
-        form_action.innerText = "WARIANT GRY";
+        form_action.innerText = "GAME WARIANT";
         const select = this.createElement('select'); 
         select.setAttribute('name', 'game variant');
         const option_first = this.createElement('option');
         option_first.setAttribute('value', 'classic');
         option_first.setAttribute('selected', "");
-        option_first.innerText ='Klasyczny';
+        option_first.innerText ='Classic';
         const option_second = this.createElement('option');
         option_second.setAttribute('value','amateur');
-        option_second.innerText ='Amatorski';
+        option_second.innerText ='Amateur';
 
         select.append(option_first, option_second);
         form_action.appendChild(select);
@@ -133,9 +133,9 @@ export default class MenuView implements IMenu {
 
     private createTimeGame(element: HTMLElement){
         const time_game = this.createElement('div', 'setting--tame');
-        time_game.innerText = 'CZAS GRY'
+        time_game.innerText = 'GAME TIME'
         const span_tg = this.createElement('span');
-        span_tg.innerText = '(W MINUTACH)';
+        span_tg.innerText = '(IN MINUTES)';
         time_game.appendChild(span_tg);
 
         const slider_box = this.createElement('div', 'setting--slider');
@@ -152,7 +152,7 @@ export default class MenuView implements IMenu {
         slider_time.setAttribute('id', 'range_display');
         slider_time.setAttribute('name', 'range_display');
         slider_time.setAttribute('for', 'range_time');
-        slider_time.textContent = `Wybrany czas gry to: ${slider.getAttribute('value')}min`
+        slider_time.textContent = `Game time is ${slider.getAttribute('value')} minutes`
         slider_box.append(slider, slider_time);
         
         element.append(time_game, slider_box);
@@ -171,7 +171,7 @@ export default class MenuView implements IMenu {
     private addTime() {
         const sliderValue = (<HTMLInputElement>document.getElementById('range')).value;
         const outputElement = <HTMLElement>document.getElementById('range_display');
-        outputElement.innerText = `Wybrany czas gry to: ${sliderValue}min`;
+        outputElement.innerText = `Game time is: ${sliderValue} minutes`;
     };
 
     display(){
