86772a479d02ca7dfe8ff76ce232f4a534f86155
pkol92 <paulina_kolasa@wp.pl>
2021-02-17 08:26:17 +0100

    add end game

--- src/ts/App/Controllers/boardController.ts
+++ src/ts/App/Controllers/boardController.ts
@@ -10,6 +10,7 @@ import SaveOfMove from "../Models/savesModels/saveOfMove";
 import BoardView from "../Views/boardView";
 import { MoveSaver } from "./moveSaver";
 import MovesList from "../Views/movesList";
+import EndGame from "../Views/endGameView";
 
 
 export default class BoardController {
@@ -58,23 +59,23 @@ export default class BoardController {
 
   public addEventListenerToButton() {
     
-    this.view.timeDisplay(5, EColor.White);
-    this.view.timeDisplay(5, EColor.Black);
+    this.view.timeDisplay(5*60, EColor.White);
+    this.view.timeDisplay(5*60, EColor.Black);
     const btnBox = document.getElementById("start__button")!;
     const timeElement = document.getElementById("range");
-    btnBox.addEventListener("click", (e) => {
+    btnBox.addEventListener("click", () => {
       this.view.timeDisplay(5, EColor.White);
       this.view.timeDisplay(5, EColor.Black);
       this.newGame(5 * 60);
     });
-    timeElement?.addEventListener("change", (e) => {
+    timeElement?.addEventListener("change", () => {
       let timeValue = parseInt((<HTMLInputElement>document.getElementById("range"))
         .value);
       
 
       this.view.timeDisplay(timeValue, EColor.White);
       this.view.timeDisplay(timeValue, EColor.Black);
-      btnBox.addEventListener("click", (e) => {
+      btnBox.addEventListener("click", () => {
         if (!timeValue || timeValue === 0) {
           timeValue = 5;
         }
@@ -393,13 +394,13 @@ export default class BoardController {
     this.stopTimer();
     let winerColor = '';
     if (winer === 'b') {
-      winerColor = "Black"
-    } else {
-      winerColor = "White"
+      winerColor = "BLACK"
+      } else {
+      winerColor = "WHITE"
     };
+    const root = document.getElementById('root');
+    const end = new EndGame(root!);
 
-    if (+process.env.DEBUG!) alert(`The winer is: ${winerColor}!`);
-    location.reload();
-    if (+process.env.DEBUG!) console.log(`NEW GAME WILL START IN 5 SEC`);
-  }
+    if (+process.env.DEBUG!) end.createWiner(winerColor);
+  };
 }
